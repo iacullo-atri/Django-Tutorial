62eaf58a3eb3fbfb7782608e4f5a5fa0343c2bd0
Nick Iacullo <iacullo@usc.edu>
2022-12-14 15:45:09 -0800

    feat(ICTRAINING-1382): finish tutorial part 2
    [ICTRAINING-1384]

--- mysite/settings.py
+++ mysite/settings.py
@@ -10,10 +10,19 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import json
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+with open(os.path.join(BASE_DIR, 'secrets.json')) as secrets_file:
+    secrets = json.load(secrets_file)
+
+
+def get_secret(key):
+    return secrets[key]
 
 
 # Quick-start development settings - unsuitable for production
@@ -31,6 +40,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'polls.apps.PollsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -75,8 +85,10 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'dev',
+        'USER': 'nicholasiacullo',
+        'PASSWORD': get_secret('database_password'),
     }
 }
 
